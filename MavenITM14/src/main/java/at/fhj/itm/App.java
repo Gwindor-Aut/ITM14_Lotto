df4b98e05c8b2e8d0d9548f75f1a884fb9003029
Bergmann Anja <anja.bergmann@edu.fh-joanneum.at>
2016-10-24 21:49:02 +0200

    maven demo

---
+++ MavenITM14/src/main/java/at/fhj/itm/App.java
@@ -0,0 +1,13 @@
+package at.fhj.itm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
