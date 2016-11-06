a466878371df1846fa91d07c416842e9b3256bb4
Heli Weithaler <helmuth.weithaler@gmail.com>
2016-11-06 15:03:16 +0100

    Try to fix FileloaderBug #2

--- ITM_Lotto/src/at/fhj/itm/utils/FileLoader.java
+++ ITM_Lotto/src/at/fhj/itm/utils/FileLoader.java
@@ -1,27 +1,42 @@
 package at.fhj.itm.utils;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Scanner;
 
 public class FileLoader {
 	
 	
 	public String getFile(String fileName){
-		StringBuilder result = new StringBuilder();
-		
-		File file = new File(Thread.currentThread().getContextClassLoader().getResource("/" + fileName).getFile());
-		
-		try (Scanner scanner = new Scanner(file)){
-			while(scanner.hasNextLine()){
-				result.append(scanner.nextLine());
-				
+		StringBuilder result = new StringBuilder("");
+		URL resource = Thread.currentThread().getContextClassLoader().getResource("/" + fileName);
+		try (InputStream in = resource.openStream()){
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(inReader);
+			String line;
+			while((line = br.readLine())!=null){
+				result.append(line);
 			}
-			scanner.close();
-			
-		} catch(IOException e){
+		} catch (IOException e){
 			e.printStackTrace();
 		}
+		
+		//File file = new File(Thread.currentThread().getContextClassLoader().getResource("/" + fileName).getFile());
+//		try (Scanner scanner = new Scanner(file)){
+//			while(scanner.hasNextLine()){
+//				result.append(scanner.nextLine());
+//				
+//			}
+//			scanner.close();
+//			
+//		} catch(IOException e){
+//			e.printStackTrace();
+//		}
 		
 		return result.toString();
 	}
