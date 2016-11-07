71736249a0b414d3fe17b47e349e2498c30a8434
Floridus <florian_magna@hotmail.com>
2016-11-07 18:09:32 +0100

    Add tipp quicktipp/manual functions

--- ITM_Lotto/src/at/fhj/itm/routes/TippService.java
+++ ITM_Lotto/src/at/fhj/itm/routes/TippService.java
@@ -2,6 +2,7 @@ package at.fhj.itm.routes;
 
 import java.sql.Date;
 import java.util.Calendar;
+import java.util.Random;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -32,8 +33,7 @@ public class TippService {
 	
 	@POST
 	@Path("/new")
-	public void createNewTipp(@FormParam("isQuicktipp") boolean isQuicktipp,
-			@FormParam("userId") int userId, @FormParam("drawId") int drawId,
+	public void createNewManualTipp(@FormParam("userId") int userId, @FormParam("drawId") int drawId,
 			@FormParam("numberOne") int numberOne,
 			@FormParam("numberTwo") int numberTwo,
 			@FormParam("numberThree") int numberThree,
@@ -41,14 +41,14 @@ public class TippService {
 			@FormParam("numberFive") int numberFive,
 			@FormParam("numberSix") int numberSix){
 		
-		System.out.println("Test::createNewTipp");
+		System.out.println("Test::createNewManualTipp");
 		
 		Date date = new Date(Calendar.getInstance().getTimeInMillis());
 		
 		String tipps = numberOne + ";" + numberTwo + ";" + numberThree + ";" + 
 				numberFour + ";" + numberFive + ";" + numberSix;
 		
-		Tipp tipp = new Tipp(0, tipps, isQuicktipp, date, userId, drawId);
+		Tipp tipp = new Tipp(0, tipps, false, date, userId, drawId);
 		
 		System.out.println(tipp);
 		
@@ -58,6 +58,56 @@ public class TippService {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@POST
+	@Path("/new")
+	public void createNewQuickTipp(@FormParam("userId") int userId,
+			@FormParam("drawId") int drawId,
+			@FormParam("quicktipps") int quicktipps){
+		
+		System.out.println("Test::createNewQuickTipp");
+		Random randomGenerator = new Random();
+		Date date = new Date(Calendar.getInstance().getTimeInMillis());
+		
+		for (int q = 0; q < quicktipps; q++) {
+			String tipps = "";
+			int[] numbers = new int[6];
+			for (int i = 0; i < 6; i++) {
+				// nextInt gibt einen Wert zwischen 0 - 44
+				int randomInt = randomGenerator.nextInt(45) + 1;
+				boolean exist = true;
+				
+				while (exist) {
+					exist = find(numbers, randomInt);
+				}
+				
+				numbers[i] = randomInt;
+				if (i != 0) {
+					tipps += ";";
+				}
+				tipps += randomInt;
+			}
+			
+			Tipp tipp = new Tipp(0, tipps, true, date, userId, drawId);
+			
+			System.out.println(tipp);
+			
+			try {
+				int tippId = tdao.create(tipp);
+				System.out.println("Tipp ID: " + tippId);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public boolean find(int[] array, int value) {
+	    for (int i = 0; i < array.length; i++) 
+	         if (array[i] == value)
+	             return true;
+	    
+	    return false;
 	}
 
 }
